457fae9eede5411d2d3756a73c44d2b0f5342f7e
zh <569184478@qq.com>
2021-06-29 10:54:48 +0800

    icon

---
+++ lib/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Icon from './icon';
+
+ReactDOM.render(<div>
+  <Icon name="qq"/>
+  <Icon name="wechat"/>
+</div>, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+module.exports = {
+  entry: {
+    index: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'ZHUI',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+      },
+    ]
+  },
+}
